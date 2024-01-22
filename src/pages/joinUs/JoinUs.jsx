0ec1ea6453bbdf46d0888a1c4eeb6362c5fa11d8
Antonio Franco <abfranco4@gmail.com>
2024-01-21 22:02:50 -0800

    Add "Ways To Run With Us" section to "Join us" page (#40)
    * split JoinUs code into sections
    
    * Ted feedback: text adjustments with new section for JoinUs
    
    * add buttons on Join us page

--- src/pages/joinUs/JoinUs.jsx
+++ src/pages/joinUs/JoinUs.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './joinUs.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle, faMeetup } from '@fortawesome/free-brands-svg-icons';
@@ -26,6 +27,8 @@ import {
   LI_SATURDAY_CLUB_RUNS,
   LI_SOCIAL_RACE_ACTIVITIES,
   MEETUP_GROUP,
+  JOIN_US_BUTTON_1,
+  JOIN_US_BUTTON_2,
 } from '../../text/JoinUs';
 import Card from '../../UI/Card';
 import {
@@ -33,6 +36,92 @@ import {
   GOOGLE_FORM_LINK,
   MEETUP_URI,
 } from '../../text/externalLinks';
+
+const sectionRunWithUs = () => (
+  <Card className="joinus__card">
+    <h2 className="h2_joinus">Ways To Run With Us</h2>
+    <ul className="ul_joinus">
+      <li className="li_joinus">
+        {JOIN_US_DESCRIPTION_1}
+        <a
+          href={MEETUP_URI}
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          {MEETUP_GROUP}
+          {' '}
+          <FontAwesomeIcon icon={faMeetup} />
+          .
+        </a>
+      </li>
+      <li className="li_joinus">{JOIN_US_DESCRIPTION_2}</li>
+      <li className="li_joinus">{JOIN_US_DESCRIPTION_3}</li>
+    </ul>
+    <Link
+      to={MEETUP_URI}
+      className="btn lg"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {JOIN_US_BUTTON_1}
+    </Link>
+  </Card>
+);
+
+const sectionBecomeMember = () => (
+  <Card className="joinus__card">
+    <h2 className="h2_joinus">Become A Member</h2>
+    <ul className="ul_joinus">
+      <li className="li_joinus">
+        {JOIN_US_DESCRIPTION_4}
+        <a
+          href={GOOGLE_FORM_LINK}
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          {GOOGLE_FORM}
+          {' '}
+          <FontAwesomeIcon icon={faGoogle} />
+          .
+          {' '}
+        </a>
+      </li>
+      <li className="li_joinus">{JOIN_US_DESCRIPTION_5}</li>
+    </ul>
+    <Link
+      to={GOOGLE_FORM_LINK}
+      className="btn lg"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {JOIN_US_BUTTON_2}
+    </Link>
+  </Card>
+);
+
+const sectionMemberBenefits = () => (
+  <Card className="joinus__card">
+    <h2 className="h2_joinus">{H2_JOIN_US}</h2>
+    <ul>
+      <li className="li_joinus">{LI_SATURDAY_CLUB_RUNS}</li>
+      <li className="li_joinus">{LI_CLUB_SUPPORT_NETWORKING}</li>
+      <li className="li_joinus">{LI_CLUB_GATHERINGS}</li>
+      <li className="li_joinus">{LI_MONTHLY_NEWSLETTERS}</li>
+      <li className="li_joinus">{LI_CLUB_LOGO_MERCHANDISE}</li>
+      <li className="li_joinus">{LI_SOCIAL_RACE_ACTIVITIES}</li>
+      <li className="li_joinus">{LI_RACE_DISCOUNT_CODES}</li>
+      <li className="li_joinus">
+        {LI_DISCOUNT_ON_SHOES}
+        <a href={ARM_URI} target="_blank" rel="noreferrer noopener">
+          {ARM}
+        </a>
+      </li>
+      <li className="li_joinus">{LI_ACCESS_TO_STRAVA_FACEBOOK}</li>
+      <li className="li_joinus">{LI_AFFORDABLE_MEMBERSHIP_FEES}</li>
+      <li className="li_joinus">{LI_EASY_ANNUAL_RENEWAL_PROCESS}</li>
+    </ul>
+  </Card>
+);
 
 function JoinUs() {
   return (
@@ -42,63 +131,11 @@ function JoinUs() {
       <section className="joinus">
         <div className="container joinus__container">
           <div className="joinus__wrapper">
-            <Card className="joinus__card">
-              <h2 className="h2_joinus">Become A Member</h2>
-              <ul>
-                <li className="li_joinus">
-                  {JOIN_US_DESCRIPTION_1}
-                  <a
-                    href={MEETUP_URI}
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    {MEETUP_GROUP}
-                    {' '}
-                    <FontAwesomeIcon icon={faMeetup} />
-                    .
-                  </a>
-                </li>
-                <li className="li_joinus">{JOIN_US_DESCRIPTION_2}</li>
-                <li className="li_joinus">{JOIN_US_DESCRIPTION_3}</li>
-                <li className="li_joinus">
-                  {JOIN_US_DESCRIPTION_4}
-                  <a
-                    href={GOOGLE_FORM_LINK}
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    {GOOGLE_FORM}
-                    {' '}
-                    <FontAwesomeIcon icon={faGoogle} />
-                    .
-                    {' '}
-                  </a>
-                </li>
-                <li className="li_joinus">{JOIN_US_DESCRIPTION_5}</li>
-              </ul>
-            </Card>
+            {sectionRunWithUs()}
             &nbsp;&nbsp;
-            <Card className="joinus__card">
-              <h2 className="h2_joinus">{H2_JOIN_US}</h2>
-              <ul>
-                <li className="li_joinus">{LI_SATURDAY_CLUB_RUNS}</li>
-                <li className="li_joinus">{LI_CLUB_SUPPORT_NETWORKING}</li>
-                <li className="li_joinus">{LI_CLUB_GATHERINGS}</li>
-                <li className="li_joinus">{LI_MONTHLY_NEWSLETTERS}</li>
-                <li className="li_joinus">{LI_CLUB_LOGO_MERCHANDISE}</li>
-                <li className="li_joinus">{LI_SOCIAL_RACE_ACTIVITIES}</li>
-                <li className="li_joinus">{LI_RACE_DISCOUNT_CODES}</li>
-                <li className="li_joinus">
-                  {LI_DISCOUNT_ON_SHOES}
-                  <a href={ARM_URI} target="_blank" rel="noreferrer noopener">
-                    {ARM}
-                  </a>
-                </li>
-                <li className="li_joinus">{LI_ACCESS_TO_STRAVA_FACEBOOK}</li>
-                <li className="li_joinus">{LI_AFFORDABLE_MEMBERSHIP_FEES}</li>
-                <li className="li_joinus">{LI_EASY_ANNUAL_RENEWAL_PROCESS}</li>
-              </ul>
-            </Card>
+            {sectionBecomeMember()}
+            &nbsp;&nbsp;
+            {sectionMemberBenefits()}
           </div>
         </div>
       </section>
--- src/text/JoinUs.js
+++ src/text/JoinUs.js
@@ -1,11 +1,15 @@
 export const JOIN_US_TITLE = 'JOIN US';
-export const JOIN_US_DESCRIPTION_1 = 'Learn about the run locations by checking our ';
-export const MEETUP_GROUP = 'Meetup Group ';
-export const JOIN_US_DESCRIPTION_2 = "You don't need to be an official MPRC club member to join our runs, but you must RSVP to the Meetup event.";
-export const JOIN_US_DESCRIPTION_3 = 'Joining Meetup and attending a run are both free of charge.';
-export const JOIN_US_DESCRIPTION_4 = 'To become an official MPRC club member, please fill out the ';
+export const JOIN_US_DESCRIPTION_1 = 'Join us through ';
+export const MEETUP_GROUP = 'Meetup ';
+export const JOIN_US_DESCRIPTION_2 = 'Joining Meetup and attending a run are both free of charge.';
+export const JOIN_US_DESCRIPTION_3 = 'You don\'t need to be an official MPRC club member to join our runs, but you must RSVP to the Meetup event.';
+export const JOIN_US_DESCRIPTION_4 = 'Become an official MPRC club member by filling out the ';
 export const JOIN_US_DESCRIPTION_5 = 'The fee is $15 per calendar year for individuals and $20 per calendar year for families.';
 export const GOOGLE_FORM = 'Google Form';
+
+export const JOIN_US_BUTTON_1 = 'RSVP on Meetup';
+export const JOIN_US_BUTTON_2 = 'Membership Form';
+
 export const H2_JOIN_US = 'Member Benefits';
 export const LI_SATURDAY_CLUB_RUNS = 'Saturday morning club runs';
 export const LI_CLUB_SUPPORT_NETWORKING = 'Club support, networking with members, guest coaches/speakers';
@@ -18,4 +22,4 @@ export const LI_DISCOUNT_ON_SHOES = '10% discount on shoes at ';
 export const LI_ACCESS_TO_STRAVA_FACEBOOK = "Access to Club's private Strava, Facebook, and WhatsApp groups";
 export const LI_AFFORDABLE_MEMBERSHIP_FEES = 'Affordable membership fees';
 export const LI_EASY_ANNUAL_RENEWAL_PROCESS = 'Easy annual renewal process';
-export const ARM = "A Runner's Mind";
+export const ARM = 'A Runner\'s Mind';
