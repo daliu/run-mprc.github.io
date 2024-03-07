d6a6fbb4a9270f7436e1df24ba01a36a2ce5f546
Rashmi Jadhav <rsjcoepit@gmail.com>
2024-03-06 21:41:05 -0800

    Addressing small changes in text per Ted's request

--- src/pages/activities/Activities.jsx
+++ src/pages/activities/Activities.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './activities.css';
 import { Link } from 'react-router-dom';
 import HeaderImage from '../../images/activities/header_bg_1.jpg';
@@ -18,45 +18,42 @@ import Extra1 from '../../images/activities/extra1.jpg';
 import Extra2 from '../../images/activities/extra2.jpeg';
 import Extra3 from '../../images/activities/extra3.jpeg';
 import FlexColumnContainer from '../../components/FlexColumnContainer';
-import { WAIVER_TEXT } from '../../text/JoinUs';
+import VideoContainer from '../../components/VideoCarousel';
 
 const brunchImages = [Brunch1, Brunch2, Brunch3];
 const activityImages = [Activity1, Activity2];
 const extraImages = [Extra1, Extra2, Extra3];
+const videos = [
+  'https://www.youtube.com/embed/Wf7stsuFcT8?si=eEHfx8PgP7NljpJW',
+  'https://www.youtube.com/embed/EsuECjg4qng?si=i5RjsOhou5sjh8Gy',
+];
 
 function Activities() {
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  const toggleExpand = () => {
-    setIsExpanded(!isExpanded);
-  };
   return (
     <>
       <Header title="Activities" image={HeaderImage} />
       <FlexColumnContainer>
         <div className="first:mt-8">
-          <Subheader title="Saturday Morning Run" />
+          <Subheader title="Saturday Morning Runs" />
         </div>
         <Paragraph>
-          Every Saturday morning, we run on the part of the Bay trail near
-          downtown San Mateo. We welcome all paces and walkers as well.
-          Non-members can run with us, but must agree to the waiver
+          Every Saturday morning, we run along the scenic Bay trail near downtown San Mateo.
+          We welcome runners of all paces as well as walkers.
+          Non-members are welcome to run with us.
+          For more details on how to run with us, check out our
           {' '}
-          <Link className="hyperlink" onClick={toggleExpand}>
-            here.
-          </Link>
-          {isExpanded && (
-            <p className="waiver-text">{WAIVER_TEXT}</p>
-          )}
+          <Link className="hyperlink" to="/joinus">Join Us</Link>
+          {' '}
+          page.
         </Paragraph>
-        <Subheader title="Brunch" />
+        <Subheader title="Brunches" />
         <Paragraph>
           &ldquo;Our informal motto is &ldquo;the eating club with a running problem&rdquo;.
           We love to eat just as much as we love to run (or more)!
           After most Saturday runs, we organize a group to try brunch at nearby places.&rdquo;
         </Paragraph>
         <ImageCarousel images={brunchImages} />
-        <Subheader title="Run Club Social" />
+        <Subheader title="Run Club Socials" />
         <Paragraph>
           Celebrate the end of each month with our Run Club Socials!
           On the last Saturday of each month, our club members bring food and drink to share.
@@ -70,6 +67,8 @@ function Activities() {
           and have costumed runs on some holidays!
         </Paragraph>
         <ImageCarousel images={extraImages} />
+        <h2 className="my-8">Experience the MPRC through these videos</h2>
+        <VideoContainer videos={videos} />
       </FlexColumnContainer>
     </>
   );
